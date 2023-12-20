2f9d415cf24ac13d459bcd74068d7e0ab2917b74
karinaPino <karinapinowork@gmail.com>
2023-12-20 17:36:41 +0100

    primera función

--- js/index.js
+++ js/index.js
@@ -6,3 +6,28 @@ let textoResultado = document.getElementById("resultado");
 let estadisticas = document.getElementById("estadisticas");
 let exitos = localStorage.getItem("exitos", "valorExito") || 0;
 let fallos = localStorage.getItem("fallos", "valorFallo") || 0;
+
+
+function crearRandomNumero() {
+    randomNumero = Math.random(Math.round()) * 10;
+}
+
+function verificarNumero() {
+    let usuarioNumero = document.getElementById("numeroUsuario").value;
+    usuarioNumero = parseInt;
+
+    if(usuarioNumero === randomNumero) {
+        exitos++;
+        localStorage.getItem("exitos");
+        textoResultado.innerHTML = "Genial!"
+        estadisticas.innerHTML = "Tienes " + exitos + " éxitos" + "y " + fallos + " fallos";
+        crearRandomNumero();
+    }  else {
+        fallos++;
+        localStorage.getItem("fallos");
+        textoResultado.innerHTML = "Has fallado"
+        estadisticas.innerHTML = "Tienes " + exitos + " éxitos" + "y " + fallos + " fallos";
+    }
+
+
+}
