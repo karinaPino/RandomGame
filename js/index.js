36d085ab441009f81562ebc6ed61031aaa2547ed
karinaPino <karinapinowork@gmail.com>
2023-12-20 17:56:58 +0100

    codigo completo pero que no funciona

--- js/index.js
+++ js/index.js
@@ -4,30 +4,35 @@ let boton = document.querySelector(".section__button");
 let botonLimpiar =  document.querySelector(".borrarDatos");
 let textoResultado = document.getElementById("resultado");
 let estadisticas = document.getElementById("estadisticas");
-let exitos = localStorage.getItem("exitos", "valorExito") || 0;
-let fallos = localStorage.getItem("fallos", "valorFallo") || 0;
+let exitos = localStorage.getItem("valorExito", "exitos") || 0;
+let fallos = localStorage.getItem("valorFallo", "fallos") || 0;
 
 
 function crearRandomNumero() {
-    randomNumero = Math.random(Math.round()) * 10;
+    randomNumero = Math.round(Math.random() * 10);
 }
 
 function verificarNumero() {
     let usuarioNumero = document.getElementById("numeroUsuario").value;
-    usuarioNumero = parseInt;
+    usuarioNumero = parseInt();
 
     if(usuarioNumero === randomNumero) {
         exitos++;
-        localStorage.getItem("exitos");
+        localStorage.getItem("valorExito", exitos);
         textoResultado.innerHTML = "Genial!"
         estadisticas.innerHTML = "Tienes " + exitos + " éxitos" + "y " + fallos + " fallos";
         crearRandomNumero();
     }  else {
         fallos++;
-        localStorage.getItem("fallos");
+        localStorage.getItem("valorFallo", fallos);
         textoResultado.innerHTML = "Has fallado"
         estadisticas.innerHTML = "Tienes " + exitos + " éxitos" + "y " + fallos + " fallos";
     }
 
-
 }
+
+function limpiarDatos() {
+    localStorage.clear();
+    location.reload();
+}
+
